3b2a61234477687a4c9920fa5fa68fdfc35c2894
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-05 21:58:11 +0800

    Add files via upload

---
+++ oddsum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class oddsum{
+    public static void main(String[] atgs){
+        long max=0;
+        long sum = 0;
+ 
+        System.out.println("Enter Maximum No.:");
+        Scanner sc = new Scanner(System.in);
+        max = sc.nextInt();
+
+        // Start of Solution
+        for(int ctr = 1; ctr < max; ctr++){
+            if(ctr % 2 != 0){
+                sum += ctr;
+            }
+        }
+        System.out.print(sum);
+        // End of Solution
+
+    }
+}
