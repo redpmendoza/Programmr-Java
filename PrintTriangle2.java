69292c8efcd1f376b89b8c80f7d965bc59a6c09a
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-07 07:54:29 +0800

    Add files via upload

---
+++ PrintTriangle2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class PrintTraingle2{
+    public static void main(String arg[]){
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the size:");
+    int size=scanner.nextInt();
+    
+    System.out.println("Triangle is:");
+
+    // Start of Solution
+    for(int ctr = 1; ctr <= size; ctr++){
+        for(int ctr2 = 1; ctr2 <= ctr; ctr2++){
+            System.out.print(ctr);
+        }
+        System.out.println("");
+    }
+    // End of Solution
+
+    }
+}
