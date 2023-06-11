540b46208de9c81e2d85208521f69a670f798b95
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-11 21:55:14 +0800

    Add files via upload

---
+++ ForloopTest1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ForloopTest1{
+    public static void main(String arg[]){
+        
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Please enter the size of the numbers:");
+    int size=scanner.nextInt();
+
+    // Start of Solution
+    
+    for(int ctr = 0; ctr <= size; ctr++){
+        if(ctr % 2 == 0){
+            System.out.println(ctr);
+        }
+    }
+    
+    // End of Solution
+
+    }
+}
