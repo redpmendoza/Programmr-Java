e0623bca0b889746613a1fa73469d9a9f9ec5cf7
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-18 21:37:26 +0800

    Add files via upload

---
+++ OperatorTest.java
@@ -0,0 +1,20 @@
+public class OperatorTest {     
+  public static void main(String[] args) {
+
+    int number, number2;  
+
+    number = 15;
+    number2 = 9;
+
+    int calculatedResult = 0;
+
+    System.out.println("Multiplication of the 2 numbers is:");
+
+    // Start of Solution
+    calculatedResult = number * number2;
+    // End of Solution
+  
+    System.out.println(calculatedResult);
+ 
+    }
+}
