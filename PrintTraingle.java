cfa69e8402ee54ad0ea31081385608c725bfe497
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-19 15:31:59 +0800

    Add files via upload

---
+++ PrintTraingle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class PrintTraingle{
+    
+    public static void main(String arg[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size:");
+        int size=scanner.nextInt();
+        System.out.println("Traingle is:");
+        print_(size);
+    }
+
+    // Start of Solution
+    public static void print_(int n){
+        for(int ctr = 0; ctr < n; ctr++){
+            for(int ctr2 = 0; ctr2 <= ctr; ctr2++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    // End of Solution
+    
+}
