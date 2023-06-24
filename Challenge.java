def61cab71115dda6e9d860e9db21202d637cfce
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-06-24 21:38:59 +0800

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+class Challenge{ 
+    public static void main(String args[]){ 
+ 
+    Scanner scanner=new Scanner(System.in); 
+    String total = ""; 
+    
+    // Start of Solution
+    String a = scanner.nextLine();
+    String b = scanner.nextLine();
+    String c = scanner.nextLine();
+    
+    total = a + b + c;
+    // End of Solution
+    
+    System.out.println(total);  
+        
+    } 
+} 
